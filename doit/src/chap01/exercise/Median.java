55d8cff4645bfe461df4b3b89dbde8c65c6fc798
ijnooyah <ijnooyah@gmail.com>
2021-11-23 22:42:08 +0900

    ch01 ex 1c-1

--- doit/src/chap01/exercise/Median.java
+++ doit/src/chap01/exercise/Median.java
@@ -1,8 +1,8 @@
-package chap01.practice;
+package chap01.exercise;
 
 import java.util.Scanner;
 
-public class Q4 {
+public class Median {
 	static int med3(int a, int b, int c) {
 		if (a >= b)  {
 			if(b >= c) {
